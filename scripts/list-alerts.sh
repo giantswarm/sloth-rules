49449338f1cb2d37cc8be484674e3cb6b85b4e51
QuantumEnigmaa <thibaud@giantswarm.io>
2024-04-18 11:08:10 +0200

    update script according to review

--- scripts/list-alerts.sh
+++ scripts/list-alerts.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Example how to run it:
-# scripts/find-alerts.sh '.labels.team=="atlas"'
+# scripts/list-alerts.sh '.labels.team=="atlas"'
 # => will report all alerts for team Atlas.
 
 # /!\ This script is provided as-is.
@@ -20,16 +20,20 @@ selectQueriesString="$(printf "| select(%s)\n" "${selectQueries[@]}")"
 
 # For each rules file
 for rulesFile in "$rulesFilesDir"/*.yaml; do
-    # Retrieve (in an array) alert names that match the query
-    mapfile -t alertsList < <(
-        yq -ojson "$rulesFile" 2>/dev/null \
-        | jq '.spec.slos[].alerting
-            '"$selectQueriesString"'
-            | .name' 2>/dev/null
-    ) || continue
+    # As of now, the slos field in the prometheusServiceLebel Resources only contains one alert because of the way those are generated with our abstraction layer.
+    # If the way we write slos was to change and multiple alerts could be created in the slos field, this would have to be updated accordingly.
+    alertList="$(yq -ojson "$rulesFile" 2>/dev/null | jq '.spec.slos[].alerting'"$selectQueriesString"' | .' 2>/dev/null)"
+    
+    if [[ $alertList != "" ]]; then
+        name="$(echo $alertList | jq -r '.name' 2>/dev/null)"
+        team="$(echo $alertList | jq -r '.labels.team?' 2>/dev/null)"
+        isSilenced="$(echo $alertList | jq -r '.pageAlert.labels.silence?' 2>/dev/null)"
 
-    # Console output
-    for alert in "${alertsList[@]}"; do
-        echo "$alert"
-    done
+        if [[ $isSilenced != "true" ]]; then
+            isSilenced="false"
+        fi
+        
+        echo "alert name : \"$name\" - team : \"$team\" - silence : \"$isSilenced\""
+    fi 
+
 done
