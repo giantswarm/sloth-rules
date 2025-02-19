33ecb8cc2f1942c641bec61ccb247a173181aa7e
QuentinBisson <quentin@giantswarm.io>
2025-02-19 13:43:31 +0100

    clean up our alert annotations

--- Makefile.custom.mk
+++ Makefile.custom.mk
@@ -2,7 +2,7 @@
 generate:
 	go run main.go
 
-test: install-tools template-chart test-opsrecipes ## Run tests
+test: install-tools template-chart test-runbooks ## Run tests
 
 install-tools:
 	scripts/tests/install-tools.sh
@@ -10,11 +10,11 @@ install-tools:
 template-chart: install-tools ## prepare the helm chart
 	scripts/tests/template-chart.sh
 
-test-opsrecipes: install-tools template-chart ## Check if opsrecipes are valid
-	scripts/tests/check-opsrecipes.sh
+test-runbooks: install-tools template-chart ## Check if runbooks are valid
+	scripts/tests/check-runbooks.sh
 
-test-ci-opsrecipes: install-tools template-chart ## Check if opsrecipes are valid in CI
-	scripts/tests/check-opsrecipes.sh --ci
+test-ci-runbooks: install-tools template-chart ## Check if runbooks are valid in CI
+	scripts/tests/check-runbooks.sh --ci
 
 sloth-validate: install-tools template-chart ## Validate sloth resources
 	scripts/tests/bin/sloth validate --input=scripts/tests/output/sloth-rules/templates/
